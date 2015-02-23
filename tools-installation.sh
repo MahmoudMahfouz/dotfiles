4a11cd273122389dfef3e930e9bbc1d8ffc0b23e
Mahmoud Mahfouz <mahmoud.mahfouz.zaza@gmail.com>
2015-02-24 01:34:25 +0200

    brew installations

--- tools-installation.sh
+++ tools-installation.sh
@@ -1,10 +1,15 @@
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install imagemagick git ant coreutils moreutils tmux vim
+brew install imagemagick git ant coreutils moreutils tmux vim cmake
+# IF INSTALLING vim using brew (latest version)
+# sudo mv /usr/bin/vim /usr/bin/oldvim
+# ln -s /usr/local/bin/vim /usr/bin/vim
+# also if using brew on multiple users
+# sudo chgrp -R brew {/usr/local,/Library/Caches/Homebrew} && sudo chmod -R g+w {/usr/local,/Library/Caches/Homebrew} && brew doctor
 brew install grc fasd spark ack
 brew tap phinze/homebrew-cask
 brew install brew-cask
-brew install Caskroom/cask/java
+brew install Caskroom/cask/java Caskroom/cask/xquartz
 brew install nginx mariadb redis elasticsearch memcached mongodb cassandra hbase couchdb hadoop
 # ctags used for vi editors
 brew install ctags-exuberant 
